9bc56e91012ea3fce2460f7c5fd0c51d45a50110
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-05-10 22:17:26 +0000

    automatically backed up by learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,6 +1,7 @@
 class Character < ActiveRecord::Base
-  belongs_to :network
+  # belongs_to :network
   belongs_to :show
-  # has_many :characters
+  belongs_to :actor
+  has_many :characters
   # has_many :actors thrue :characters
 end
