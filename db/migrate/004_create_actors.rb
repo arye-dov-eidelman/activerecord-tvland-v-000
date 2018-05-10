e4eb13443893a84ec766ae5d0ec8d536faafba71
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-05-10 21:42:50 +0000

    automatically backed up by learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,8 +1,8 @@
-class CreateShows < ActiveRecord::Migration[5.1]
+class CreateActors < ActiveRecord::Migration[5.1]
   def change
     create_table :actors do |t|
-      t.string :call_letters
-      t.integer :channel
+      # t.string :call_letters
+      # t.integer :channel
     end
   end
 end
