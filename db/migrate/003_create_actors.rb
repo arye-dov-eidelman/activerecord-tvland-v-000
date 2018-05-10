911a80a5976f61b45beaf4a36bfec18e7fe63a85
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-05-10 21:53:37 +0000

    automatically backed up by learn

--- db/migrate/003_create_actors.rb
+++ db/migrate/003_create_actors.rb
@@ -2,7 +2,7 @@ class CreateActors < ActiveRecord::Migration[5.1]
   def change
     create_table :actors do |t|
       t.string :first_name
-      t.integer :last_name
+      t.string :last_name
     end
   end
 end
